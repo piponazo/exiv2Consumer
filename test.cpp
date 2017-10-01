35fc7a5f80423bddf51e523e7f4f34cbf0adb52d
Luis Díaz Más <piponazo@gmail.com>
2017-10-01 18:24:16 +0200

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <exiv2/image.hpp>
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello Exiv2 world!\n";
+
+    Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(argv[1]);
+    image->readMetadata();
+
+    std::cout << "Image metadata read!\n";
+
+    return 0;
+}
